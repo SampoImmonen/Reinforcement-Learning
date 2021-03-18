1dc5fc4fff48e0a053446030407696a5f29f3047
SampoImmonen <immonensampo@gmail.com>
2021-03-18 23:33:44 +0200

    working library version

--- test_script.py
+++ test_script.py
@@ -1,121 +1,69 @@
-import torch
-import torch.nn as nn
-import gym
-import torch.multiprocessing as mp
 import time
-from collections import namedtuple
-from models import DuelingNoisyConvNet
+import torch
+
 from wrappers import make_env_basic
-from utils import Experience, PlayedEpisode, ReplayBuffer
+from utils import ExperienceSource
+from models import DuelingNoisyConvNet
 from dqn import calculate_loss
+from logger import Logger
 
-def play_proc3(net, queue):
-    env = make_env_basic('PongNoFrameskip-v4')
-    state = env.reset()
-    print("init process")
-    episode_reward = 0
-    num_steps = 0
-    net.cuda()
-    while True:
-        obs_tens = torch.tensor(state).unsqueeze(0).float().to("cuda")
-        with torch.no_grad():
-            act = net(obs_tens).max(1)[1]
-        
-        next_state, reward, isdone, _ = env.step(act.item())
-
-        episode_reward+=reward
-        num_steps +=1
-
-        exp = Experience(state, act.item(), next_state, reward, isdone)
-        queue.put(exp)
-
-        if isdone:
-            next_state = env.reset()
-            queue.put(PlayedEpisode(episode_reward, num_steps))
-            episode_reward=0
-            num_steps=0
-        state = next_state
-
-def train(net, exp_queue):
-    
-
-    sync_interval = 1000
-    net.cuda()
-    target_net = DuelingNoisyConvNet((4,84,84), 6).cuda()
-    target_net.load_state_dict(net.state_dict())
-    optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
-
-    buffer = ReplayBuffer(capacity=50000)
-
-    threshold = 10000
-    batch_size = 32*4
-    gamma = 0.99    
-    
-    tracker = EpisodeTracker(10, 15)
-    training_steps = 0
-
-    while True:
-        exp = exp_queue.get()
-        if isinstance(exp, PlayedEpisode):
-            tracker.push(exp)
-
-        else:
-            buffer.push(exp)
-
-        if len(buffer) < threshold:
-            continue
-
-        batch = buffer.sample(batch_size, as_tensor=True, device="cuda")
-
-        optimizer.zero_grad()
-        loss = calculate_loss(batch, net, target_net, gamma)
-        loss.backward()
-        optimizer.step()
-        training_steps+=1
-        if training_steps%sync_interval==0:
-            target_net.load_state_dict(net.state_dict())
-
-
-class EpisodeTracker:
-
-    def __init__(self, interval, success_score):
-
-        self.interval = interval
-        self.success_score = success_score
-
-        self.t1 = time.time()
-        self.episodes = []
-
-    def push(self, episode):
-        self.episodes.append(episode)
-        if len(self.episodes)%self.interval == 0:
-            mean = sum([episode.reward for episode in self.episodes[-self.interval:]])/self.interval
-            completed_episodes = sum([ep.reward > self.success_score for ep in self.episodes[-self.interval:]])
-            print(f"episodes played: {len(self.episodes)} mean reward:{mean}, completed episodes: {completed_episodes} time:{time.time()-self.t1}")
-
-        return 
 
 
 if __name__ == '__main__':
 
-    #warnings.simplefilter("ignore", category=UserWarning)
-    print("starting script")
-    mp.set_start_method('spawn')
+    # Add CLI
 
-    #input and output size for pong
-    net = DuelingNoisyConvNet((4,84,84), 6)
-
-    
-    net.share_memory()
-
+    #define parameters
+    print("initializing parameters")
+    device = "cuda"
     
 
-    # Shared queue and playing process
-    exp_queue = mp.Queue(maxsize=8)
-    play_proc = mp.Process(target=play_proc3, args=(net, exp_queue))
-    train_proc = mp.Process(target=train, args=(net, exp_queue))
-    play_proc.start()
-    train_proc.start()
+    env = make_env_basic("BoxingNoFrameskip-v4", crop=(30,30))
+    
+    output_dim = env.action_space.n
 
-    play_proc.join()
-    train_proc.join()
+    net = DuelingNoisyConvNet((4, 84,84), output_dim).to(device)
+    target_net = DuelingNoisyConvNet((4, 84,84), output_dim).to(device)
+    target_net.load_state_dict(net.state_dict())
+
+    optimizer = torch.optim.Adam(net.parameters(), lr=0.0001)
+
+    threshold = 10000
+    sync_interval = 1000
+    gamma  = 0.99
+
+    experience_source = ExperienceSource(env, capacity=50000, device=device)
+    
+    best_reward = 0
+    batch_size = 32
+    num_iters = 0
+    episode_rewards = []
+    #info_interval = 10
+
+    t1 = time.time()
+
+    logger = Logger("BoxingBasic", 10, complete_limit=50)
+
+
+    print("starting training")
+    while True:
+        #print("starting iter")
+        episode_reward = experience_source.step(net)
+
+        logger.push_episode(episode_reward, net)        
+
+        if experience_source.get_steps() < threshold:
+            continue
+        
+        batch = experience_source.sample(batch_size, as_tensor=True)
+        
+        optimizer.zero_grad()
+        loss = calculate_loss(batch, net, target_net, gamma, doubleQ=False)
+        loss.backward()
+        optimizer.step()
+
+        logger.push_loss(loss.item())
+
+        num_iters+=1
+        if num_iters%sync_interval==0:
+            target_net.load_state_dict(net.state_dict())
--- utils.py
+++ utils.py
@@ -1,9 +1,12 @@
 from collections import deque, namedtuple
-import random
 import numpy as np
+import random
 import torch
+import torch.nn as nn
 
-#namedtuple to store an experience tuple used to train a DQN
+
+#namedtuples to store an experience tuple used to train a DQN
+ActionPair = namedtuple('ActionPair', ('state', 'action', 'next_state', 'reward'))
 Experience = namedtuple('Experience', ('state', 'action', 'next_state', 'reward', 'isdone'))
 PlayedEpisode = namedtuple('PlayedEpisode', ('reward', 'num_steps'))
 
@@ -35,3 +38,73 @@ class ReplayBuffer:
                    )
         return states, actions, next_states, rewards, isdones
 
+
+
+class ExperienceSource:
+    
+    def __init__(self, env, capacity=10000, device="cpu"):
+        
+        self.env = env
+        self.state = self.env.reset()
+            
+        self.buffer = ReplayBuffer(capacity)
+        
+        self.device = device
+        
+        self.episode_steps = 0
+
+        self.steps_done = 0
+        self.episode_reward = 0
+        
+    def step(self, net):
+        """
+        takes a step in the environment and stores in the Replay Buffer
+        returns episode reward is episode ends
+        """
+
+        state = self.state
+        state_tensor  = torch.FloatTensor(state).unsqueeze(0).to(self.device)
+            
+
+
+        with torch.no_grad():
+            act = net(state_tensor).max(1)[1].item()
+        
+        obs, reward, isdone, _ = self.env.step(act)
+        exp = Experience(state, act, obs, reward, isdone)
+        self.buffer.push(exp)
+        
+        self.episode_steps+=1
+        self.steps_done+=1
+        
+        self.episode_reward+=reward
+        if isdone:
+            self.state = self.env.reset()
+            episode_reward = self.episode_reward
+            episode_steps = self.episode_steps
+
+            self.episode_reward = 0
+            self.episode_steps = 0
+
+            return (episode_reward, episode_steps)
+        
+        self.state = obs
+        
+        return None
+        
+    def get_steps(self):
+        return self.steps_done
+        
+            
+    def sample(self, batch_size, as_tensor=False):
+        """
+        samples a batch from ReplayBuffer and transfers to device
+        """
+        states, acts, next_states, rewards, isdones = self.buffer.sample(batch_size, as_tensor)
+        if as_tensor:
+            return (states.to(self.device), acts.to(self.device),
+                   next_states.to(self.device), rewards.to(self.device),
+                   isdones.to(self.device)
+                  )
+        else:
+            return (states, acts, next_states, rewards, isdones)
